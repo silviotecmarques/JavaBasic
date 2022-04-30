00b48dff1f99efa6c5c342128a76d1485d5d8ddb
Silvio Marques <silviotec.marques@gmail.com>
2022-04-30 19:19:44 -0300

    Atualizar

--- src/com/loiane/cursojava/aula26/Carro.java
+++ src/com/loiane/cursojava/aula26/Carro.java
@@ -1,2 +1,15 @@
-package com.loiane.cursojava.aula26;public class Carro {
+package com.loiane.cursojava.aula26;
+
+public class Carro {
+
+    String marca;
+    String modelo;
+    int numPassageiro;
+    double capCambustivel;
+    double consumoCombustivel;
+
+    double exibirAutonomia() {
+        return capCambustivel * consumoCombustivel;
+    }
+
 }
